ebc581f4ff7d99af1f2ef2ac21ec4b3a1045a406
wangke <49186456@qq.com>
2020-01-14 20:47:08 +0800

    增加一个方法

--- demo-jse/demo-jse-8/src/main/java/wava/util/optional/DemoOptional.java
+++ demo-jse/demo-jse-8/src/main/java/wava/util/optional/DemoOptional.java
@@ -1,13 +1,25 @@
 package wava.util.optional;
 
+import java.util.List;
 import java.util.Optional;
 
 public class DemoOptional {
 
 	public static void main(String[] args) {
+		testIsPresent2();
+	}
+	
+	public static void testIsPresent() {
 		Optional<String> fullName = Optional.ofNullable(null);
 		System.out.println("Full Name is " + fullName.isPresent());
 		System.out.println("Full Name is " + fullName.orElseGet(() -> "[none]"));
+	}
+	
+	public static void testIsPresent2() {
+		List<String> myList = null;
+		Optional.ofNullable(myList).ifPresent(item -> {
+			System.out.print(item);
+		});
 	}
 	
 }
