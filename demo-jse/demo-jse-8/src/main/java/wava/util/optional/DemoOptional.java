9600af0ae4bc4f80b74dbf96c8f1ef3d0f038bfe
wangke <49186456@qq.com>
2020-01-18 19:21:03 +0800

    增加一个示例

--- demo-jse/demo-jse-8/src/main/java/wava/util/optional/DemoOptional.java
+++ demo-jse/demo-jse-8/src/main/java/wava/util/optional/DemoOptional.java
@@ -6,20 +6,35 @@ import java.util.Optional;
 public class DemoOptional {
 
 	public static void main(String[] args) {
-		testIsPresent2();
+		testIsPresent3();
 	}
-	
+
 	public static void testIsPresent() {
 		Optional<String> fullName = Optional.ofNullable(null);
 		System.out.println("Full Name is " + fullName.isPresent());
 		System.out.println("Full Name is " + fullName.orElseGet(() -> "[none]"));
 	}
-	
+
 	public static void testIsPresent2() {
 		List<String> myList = null;
 		Optional.ofNullable(myList).ifPresent(item -> {
 			System.out.print(item);
 		});
 	}
-	
+
+	public static void testIsPresent3() {
+		UserDO userDO = null;
+		
+		//Optional<UserDO> uOptional = Optional.ofNullable(userDO);
+		
+		UserDTO userDTO = Optional.ofNullable(userDO).map(uDO -> {
+			UserDTO uDTO = new UserDTO();
+			uDTO.setId(uDO.getId());
+			uDTO.setName(uDO.getName());
+			return uDTO;
+		}).orElseThrow(() -> new RuntimeException("uDTO is null."));
+
+		System.out.println("userDTO = " + userDTO);
+	}
+
 }
